8371589f663fcfe7abdfd36413a1ea2dcbbee624
Tundesamson26 <odewoletundesamson@gmail.com>
2023-11-30 18:57:39 +0100

    latest commmit

--- function/invoice-generator-client/src/App.svelte
+++ function/invoice-generator-client/src/App.svelte
@@ -6,36 +6,22 @@
   import MainDetails from "./components/MainDetails.svelte";
   import Notes from "./components/Notes.svelte";
   import Table from "./components/Table.svelte";
+
+  const handlePrint = () =>{
+    window.print();
+  }
 </script>
 
 <main class="m-5 p-5 xl:max-w-4xl xl:mx-auto bg-white rounded shadow">
-  <!-- header section -->
-  <Header />
-  <!-- End of header sections -->
-
-  <!-- your details section -->
-  <MainDetails />
-  <!-- end of your detials -->
-
-  <!-- client's details  -->
-  <ClientDetails />
-  <!-- end of client's details  -->
-
-  <!-- dates sections -->
-  <Dates />
-  <!-- end od dates sections -->
-
-  <!-- Table sections -->
-  <Table />
-  <!-- End of Table sections -->
-
-  <!-- Notes -->
-  <Notes />
-  <!-- End of Notes -->
-
-  <!-- Footer sections -->
-  <Footer />
-  <!-- End of footer sections -->
+  <div>
+    <Header />
+    <MainDetails />
+    <ClientDetails />
+    <Dates />
+    <Table />
+    <Notes />
+    <Footer />
+  </div>
 </main>
 
 <style>
