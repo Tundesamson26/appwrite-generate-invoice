ea939328584fe7221456195b2a00a4997112203f
Tundesamson26 <odewoletundesamson@gmail.com>
2023-12-03 15:37:15 +0100

    updated commit

--- function/invoice-generator-client/src/App.svelte
+++ function/invoice-generator-client/src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { onMount } from "svelte";
+
   import ClientDetails from "./components/ClientDetails.svelte";
   import Dates from "./components/Dates.svelte";
   import Footer from "./components/Footer.svelte";
@@ -7,20 +9,53 @@
   import Notes from "./components/Notes.svelte";
   import Table from "./components/Table.svelte";
 
-  const handlePrint = () =>{
+  let showInvoice = false;
+
+  const handlePrint = () => {
     window.print();
-  }
+  };
+
+  const togglePreview = () => {
+    showInvoice = !showInvoice;
+  };
+
+  // Optionally, you can use onMount to set up any initial conditions
+  onMount(() => {
+    // You can add any initialization logic here
+  });
 </script>
 
 <main class="m-5 p-5 xl:max-w-4xl xl:mx-auto bg-white rounded shadow">
   <div>
-    <Header />
-    <MainDetails />
-    <ClientDetails />
-    <Dates />
-    <Table />
-    <Notes />
-    <Footer />
+    {#if showInvoice}
+      <!-- Show Invoice -->
+      <Header {handlePrint} />
+      <MainDetails />
+      <ClientDetails />
+      <Dates />
+      <Table />
+      <Notes />
+      <Footer />
+      <button
+        class="mt-5 bg-blue-500 py-2 px-8 text-white font-bold shadow border-2 border-blue-500 hover:bg-transparent hover:text-blue-500 transition-all duration-300"
+        on:click={togglePreview}>Edit Invoice</button
+      >
+    {:else}
+      <!-- Show Input Element and Preview Invoice Button -->
+      <div class="flex flex-col justify-center">
+        <input
+          type="text"
+          id="text"
+          name="text"
+          placeholder="Enter data for preview"
+          autocomplete="off"
+        />
+        <button
+          class="bg-blue-500 py-2 px-8 text-white font-bold shadow border-2 border-blue-500 hover:bg-transparent hover:text-blue-500 transition-all duration-300"
+          on:click={togglePreview}>Preview Invoice</button
+        >
+      </div>
+    {/if}
   </div>
 </main>
 
--- function/invoice-generator-client/src/components/Header.svelte
+++ function/invoice-generator-client/src/components/Header.svelte
@@ -1,4 +1,6 @@
-<script></script>
+<script>
+   export let handlePrint;
+</script>
 
 <header
   class="flex flex-col items-center justify-center mb-5 xl:flex-row xl:justify-between"
@@ -8,7 +10,7 @@
   </div>
   <div>
     <ul class="flex flex-wrap items-center justify-between">
-      <li><button class="btn btn-print">Print</button></li>
+      <li><button type="button" class="btn btn-print" on:click={handlePrint}>Print</button></li>
       <li><button class="btn btn-download">Download</button></li>
       <li><button class="btn btn-send">Send</button></li>
     </ul>
