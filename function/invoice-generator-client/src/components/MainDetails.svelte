6c9375297f3e1b5361221cc05dfceb20f13bd504
Tundesamson26 <odewoletundesamson@gmail.com>
2023-12-06 09:44:14 +0100

    updated commit

--- function/invoice-generator-client/src/components/MainDetails.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  export let name;
-  export let address;
-</script>
-
-<section class="flex flex-col items-end justify-end">
-  <h2 class="text-xl font-bold uppercase md:text-4xl">{$name}</h2>
-  <p>{$address}</p>
-</section>
