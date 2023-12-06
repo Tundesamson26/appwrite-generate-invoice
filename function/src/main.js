daaa041156f7a5e7a3ad7745f55c8a06592e0623
Tundesamson26 <odewoletundesamson@gmail.com>
2023-12-06 12:40:45 +0100

    updated commit

--- function/src/main.js
+++ function/src/main.js
@@ -1,55 +1,13 @@
 import { createPdf } from './pdf.js';
-// import { generateFakeOrder } from './faker.js';
-// import { Buffer } from 'node:buffer';
+import { generateFakeOrder } from './faker.js';
 
-export default async ({ res, log, data }) => {
-  const {
-    name,
-    address,
-    email,
-    phone,
-    bankName,
-    bankAccount,
-    website,
-    clientName,
-    clientAddress,
-    invoiceNumber,
-    invoiceDate,
-    dueDate,
-    notes,
-    list,
-  } = data;
+export default async ({ res, log }) => {
+  const fakeOrder = generateFakeOrder();
+  log(`Generated fake order: ${JSON.stringify(fakeOrder, null, 2)}`);
 
-  const pdfBuffer = await createPdf({
-    name,
-    address,
-    email,
-    phone,
-    bankName,
-    bankAccount,
-    website,
-    clientName,
-    clientAddress,
-    invoiceNumber,
-    invoiceDate,
-    dueDate,
-    notes,
-    list,
-  });
-
+  const pdfBuffer = await createPdf(fakeOrder);
   log('PDF created.');
 
   return res.send(pdfBuffer, 200, { 'Content-Type': 'application/pdf' });
 };
-
-
-// export default async ({ res, log }) => {
-//   const fakeOrder = generateFakeOrder();
-//   log(`Generated fake order: ${JSON.stringify(fakeOrder, null, 2)}`);
-
-//   const pdfBuffer = await createPdf(fakeOrder);
-//   log('PDF created.');
-
-//   return res.send(pdfBuffer, 200, { 'Content-Type': 'application/pdf' });
-// };
 
