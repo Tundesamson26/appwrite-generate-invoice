ab928166cc06ad1d743d7c1646420eea77884135
Tundesamson26 <odewoletundesamson@gmail.com>
2023-12-06 22:44:58 +0100

    updated commit

--- function/src/main.js
+++ function/src/main.js
@@ -12,7 +12,6 @@
 // };
 
 import { createPdf } from './pdf.js';
-// import { generateFakeOrder } from './faker.js';
 // import { Buffer } from 'node:buffer';
 
 export default async ({ res, req, log }) => {
@@ -20,7 +19,7 @@ export default async ({ res, req, log }) => {
   const pdfBuffer = await createPdf(req.body);
 
   log('PDF created.');
-    // log(req.body)
+    log(req.body)
 
   return res.send(pdfBuffer, 200, { 'Content-Type': 'application/pdf' });
   // return res.send(req.body)
