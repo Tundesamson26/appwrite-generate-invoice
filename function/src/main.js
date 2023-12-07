2518c41d161afa6a7125b87fa4c13d0453f095fe
Tundesamson26 <odewoletundesamson@gmail.com>
2023-12-07 12:34:29 +0100

    paylosd list commit

--- function/src/main.js
+++ function/src/main.js
@@ -3,7 +3,7 @@ import { createPdf } from './pdf.js';
 export default async ({ res, req, log, error }) => {
   const payload = req.body;
   
-  log(payload.list);
+  log(typeof payload);
 
   // const payload = {
   //   name: "tunde",
