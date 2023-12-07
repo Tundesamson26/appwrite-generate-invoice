587c148e28ecf971dc1d0fb58ce8dbd91ab767d7
Tundesamson26 <odewoletundesamson@gmail.com>
2023-12-07 11:38:19 +0100

    cleanup main function

--- function/src/main.js
+++ function/src/main.js
@@ -1,16 +1,3 @@
-// import { createPdf } from './pdf.js';
-// import { generateFakeOrder } from './faker.js';
-
-// export default async ({ res, log }) => {
-//   const fakeOrder = generateFakeOrder();
-//   log(`Generated fake order: ${JSON.stringify(fakeOrder, null, 2)}`);
-
-//   const pdfBuffer = await createPdf(fakeOrder);
-//   log('PDF created.');
-
-//   return res.send(pdfBuffer, 200, { 'Content-Type': 'application/pdf' });
-// };
-
 import { createPdf } from './pdf.js';
 
 export default async ({ res, req, log, error }) => {
@@ -42,8 +29,6 @@ export default async ({ res, req, log, error }) => {
   const pdfBuffer = await createPdf(payload);
 
   log('PDF created.');
-  
 
   return res.send(pdfBuffer, 200, { 'Content-Type': 'application/pdf' });
-  // return res.send(req.body)
 };
