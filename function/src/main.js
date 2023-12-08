d32006394043f781781218f528377f32d3d5ab7e
Tundesamson26 <odewoletundesamson@gmail.com>
2023-12-08 21:23:32 +0100

    tested commit

--- function/src/main.js
+++ function/src/main.js
@@ -15,7 +15,7 @@ export default async ({ res, req, log, error }) => {
 
       return res.send(pdfBase64, 200, { "Content-Type": "application/pdf" });
     } catch (err) {
-      error('Error processing the request:', err.message);
+      error('Error processing the request:', err);
       return res.send('Internal Server Error');
     }
   } else {
