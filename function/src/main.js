339e6501e1b50ea5c492458e6599694af2e95d23
Tundesamson26 <odewoletundesamson@gmail.com>
2023-12-11 14:54:45 +0100

    bad testing

--- function/src/main.js
+++ function/src/main.js
@@ -7,27 +7,30 @@ export default async ({ res, req, log, error }) => {
   // req.headers('Access-Control-Allow-Methods', 'OPTIONS, POST');
   // req.headers('Access-Control-Allow-Headers', 'Content-Type');
 
-log(req.headers['Access-Control-Allow-Origin'] === '*'); 
+   
   
-  // if (req.method === "POST" && req.headers['Access-Control-Allow-Origin'] === '*') {
-  //   try {
-  //     const payload = querystring.parse(req.body);
+  if (req.method === "POST" && req.headers['access-control-allow-origin'] === '*') {
+    try {
 
-  //     const pdfBuffer = await createPdf(payload);
+      log(req.headers['access-control-allow-origin'] === '*'); 
+       return res.send('correct')
+      // const payload = querystring.parse(req.body);
 
-  //     log(payload);
+      // const pdfBuffer = await createPdf(payload);
 
-  //     const pdfBase64 = pdfBuffer.toString('base64');
+      // log(payload);
 
-  //     return res.send(pdfBase64, 200, { "Content-Type": "application/pdf" });
-  //   } catch (err) {
-  //     error('Error processing the request:', err);
-  //     return res.send('Internal Server Error');
-  //   }
-  // } else {
-  //   log(error)
-  //   return res.send('Bad Request');
-  // }
+      // const pdfBase64 = pdfBuffer.toString('base64');
+
+      // return res.send(pdfBase64, 200, { "Content-Type": "application/pdf" });
+    } catch (err) {
+      error('Error processing the request:', err);
+      return res.send('Internal Server Error');
+    }
+  } else {
+    log(error)
+    return res.send('Bad Request');
+  }
 
 };
 
